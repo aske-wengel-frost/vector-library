a4758d4f26ea0a0ae8e35b8c957425b9cfa60766
g1tte <108462358+g1tte@users.noreply.github.com>
2023-06-01 14:26:37 +0200

    first

---
+++ vecLibrary.py
@@ -0,0 +1,78 @@
+from __future__ import annotations
+from math import cos, sin, atan, radians, sqrt
+import matplotlib.pyplot as plt
+from matplotlib.patches import Wedge
+
+# Converts string to float.
+# There may occur errors during rounding. This i caused by python's rounding-method.
+def strToFloat(s: str):  # Takes string
+    stringSplit: list[str] = s.split(".")
+    if len(stringSplit) == 1:
+        return int([0]) + 0.0  # "+0.0" converts given input to string
+    return int(stringSplit[0]) + sum([int(stringSplit[1][_-1]) / 10**_ for _ in range(1, len(stringSplit[1])+1)]) # Takes sum of 10th, 100th and 1000th and so on.
+
+class Point:
+    def __init__(self, x: float, y: float):
+        """Initialize a point using (x) and (y)-coordinates."""
+        self.x = x
+        self.y = y
+
+    # Returns value or item of given index.
+    def __getitem__(self, key: int) -> float:
+        """Returns value or item of given index."""
+        if key == 0:
+            return self.x
+        if key == 1:
+            return self.y
+        raise IndexError
+
+    def __setitem__(self, key: int, value: float):
+        """Makes sure item is within range of the init-method."""
+        if key > 1:
+            raise IndexError
+        if key == 0:
+            self.x = value
+        if key == 1:
+            self.y = value
+
+    def __str__(self) -> str:
+        """Returns string with 2 digits."""
+        return f"({self.x:2.f};{self.y:2.f})"
+
+class Vector:
+    '''
+    Takes given input in form of points (using the class: Points), or length and angle in order to calculate or visualize.
+    '''
+    def __init__(self, pos: Point = None, end: Point = None, length: float = None, angle: float = None, colour: str = "red"):
+        """
+        Create a vector using either length and angle, start- and endpoint or x- and y-length.
+        This method takes minimum amount of information.
+
+        Examples: 
+        // Position vectors: Vector(pos: Point) Vector(length: float, angle: float) // Connection vectors:
+        Vector(pos: Point, end: Point)
+        """
+        self.colour = colour
+        self.pos = pos
+        self.end = end
+        self.length = length
+        self.angle = angle
+
+        # Angle and length
+        if angle is not None:
+            if self.pos == None: 
+                self.pos = Point(0,0)
+            self.end = Point(self.pos.x + (length * cos(radians(angle))), 
+                             self.pos.y + (length * sin(radians(angle))))
+
+        elif end is None: 
+            self.pos = Point(0,0)
+            self.end = Point(pos.x, pos.y)
+        self.x = self.end.x-self.pos.x
+        self.y = self.end.y-self.pos.y
+        self.length = sqrt(self.x**2+self.y**2) # calculates length using pythagoras theorem.
+
+def __str__(self) -> str:
+    '''Returns a string of the given vector's value rounded.'''
+    return()
+
