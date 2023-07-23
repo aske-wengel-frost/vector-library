7631b91cca832233f49736e6a979f26b0dbc437a
askefrost26@gmail.com <askefrost26@gmail.com>
2023-07-23 20:46:03 +0200

    added windowtitle + testing of vector-classmethod

--- runfile.py
+++ runfile.py
@@ -1,22 +1,33 @@
-from vecLibrary import Vector, Point, scalar_product, determinant, __eq__
+from vecLibrary import Vector, Point, scalar_product, determinant, __eq__,  __str__
 import matplotlib.pyplot as plt
 
 vec_a = Vector(Point(2,3))
-vec_b = Vector(Point(4,8))
+vec_b = Vector(Point(4,8), color="blue")
 
-vec_c = Vector(Point(2,5))
-vec_d = Vector(Point(2,5))
+vec_c = Vector(Point(2,5), color="purple")
+vec_d = Vector(Point(2,5), color="purple")
 
 print(scalar_product(vec_a, vec_b))
 print(determinant(vec_a, vec_b))
 print(__eq__(vec_c, vec_d))
 
+point_1 = Point(1,2)
+x_coordinate = point_1.__getitem__(0)
+print("point_1's x-coordinate:", x_coordinate)
+
 # Create a and axes for visualization
 fig, ax = plt.subplots()
+vec_a.render(fig, ax)
+vec_b.render(fig, ax)
+vec_c.render(fig, ax)
 vec_d.render(fig, ax)
 
 # Specify plot limitations
 plt.xlim(-1, 6)
 plt.ylim(-1, 10)
 plt.grid()
+
+# Set the window title
+# without '.manger' if running on windows.
+plt.gcf().canvas.manager.set_window_title("Vector visualization")
 plt.show()
