a6426b9f92f4980c825893ecdeb035cc5cdab5e5
askefrost26@gmail.com <askefrost26@gmail.com>
2023-07-23 19:22:35 +0200

    Trying out dundermethod + new function(determinant)

--- runfile.py
+++ runfile.py
@@ -1,5 +1,10 @@
-from vecLibrary import Vector, Point, scalar_product
+from vecLibrary import Vector, Point, scalar_product, determinant, __eq__
 vec_a = Vector(Point(2,3))
 vec_b = Vector(Point(4,8))
 
+vec_c = Vector(Point(2,5))
+vec_d = Vector(Point(2,5))
+
 print(scalar_product(vec_a, vec_b))
+print(determinant(vec_a, vec_b))
+print(__eq__(vec_c, vec_d))
