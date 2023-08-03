250304808164345eb453e20c24909ee5c5c08991
Aske Wengel Frost <askewengelfrost@Askes-MacBook-Air.local>
2023-08-03 16:22:26 +0200

    Describtions for all actions within the runfile

--- runfile.py
+++ runfile.py
@@ -9,17 +9,19 @@ from vecLibrary import (
     difference,
 )
 
+# Create vectors
 vec_a = Vector(Point(2, 3))
 vec_b = Vector(Point(4, 8), color="blue")
-
 vec_c = Vector(Point(2, 5), color="purple")
 vec_d = Vector(Point(2, 5), color="purple")
 
+# Trying out the different functions
 print(scalar_product(vec_a, vec_b))
 print(determinant(vec_a, vec_b))
 print(__eq__(vec_c, vec_d))
 print(difference(vec_b, vec_c))
 
+# Trying out the __getitem__ function
 point_1 = Point(1, 2)
 x_coordinate = point_1.__getitem__(0)
 print("point_1's x-coordinate:", x_coordinate)
